755a53e32f8f6fb79f17544e8ebd21ef550ba66d
Yevgeniya <yevgeniya-an@mail.ru>
2020-10-20 23:36:09 +0600

    feat: implement task solution

--- src/index.js
+++ src/index.js
@@ -26,21 +26,41 @@ const MORSE_TABLE = {
     '-.--':   'y',
     '--..':   'z',
     '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+const MAPPING = {
+    '10': '.',
+    '11': '-'
+};
+
+const SPACE = '**********';
+
 function decode(expr) {
-    // write your solution here
+    let res = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        const letter = expr.substring(i, i + 10);
+        if (letter === SPACE) {
+            res += ' ';
+            continue;
+        }
+        let code = '';
+        for (let j = letter.indexOf('1'); j < letter.length; j += 2) {
+            code += MAPPING[letter.substring(j, j + 2)] || '';
+        }
+        res += MORSE_TABLE[code];
+    }
+    return res;
 }
 
 module.exports = {
     decode
-}
+};
